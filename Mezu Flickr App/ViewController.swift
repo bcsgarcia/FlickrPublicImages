b85d2c92cfbea495adcb69366648a3c80eabaca9
Bruno Garcia <bcsgarcia@outlook.com>
2019-07-16 10:54:11 -0300

    Initial Commit

---
+++ Mezu Flickr App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Mezu Flickr App
+//
+//  Created by Garcia, Bruno (B.C.) on 16/07/19.
+//  Copyright © 2019 Garcia, Bruno (B.C.). All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
